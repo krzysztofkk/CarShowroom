f4a92d7a6c5756e0c54c14bd7bff93afe5c549f3
Krzysztof Kulewski <kkulewski@kuznia.co>
2016-03-13 01:10:06 +0100

    Database mock with some random data

---
+++ CarShowroom/Examples/TestDataset.cs
@@ -0,0 +1,83 @@
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Specialized;
+using System.IO;
+using CarShowroom.Database;
+
+namespace CarShowroom.Examples
+{
+	public static class TestDataset
+	{
+		//some contact people for brands
+		public static Person contact1 = new Person("Harald Neuer", "601-602-603");
+		public static Person contact2 = new Person("Stefano Gino", "343-121-777");
+		public static Person contact3 = new Person("John Kreuger", "545-300-200");
+
+		//example brands
+		public static CarBrand bmw = new CarBrand(1, "BMW", "Germany", contact1);
+		public static CarBrand audi = new CarBrand(2, "Audi", "Germany", contact1);
+		public static CarBrand ferrari = new CarBrand(3, "Ferrari", "Italy", contact2);
+		public static CarBrand ford = new CarBrand(4, "Ford", "USA", contact3);
+
+		//example car models
+		public static CarModel bmw_e32 = new CarModel(bmw, "E32", 1992);
+		public static CarModel bmw_x5 = new CarModel(bmw, "X5", 2014);
+		public static CarModel audi_a8 = new CarModel(audi, "A8", 2011);
+		public static CarModel ferr_458 = new CarModel(ferrari, "458", 2013);
+		public static CarModel ford_mus = new CarModel(ford, "Mustang GT", 1971);
+		public static CarModel ford_foc = new CarModel(ford, "Focus ST", 2015);
+
+		//example cars
+		public static Car car1 = new Car(bmw_e32, "A1B22C9123");
+		public static Car car2 = new Car(bmw_x5, "G34FG21EF7");
+		public static Car car3 = new Car(audi_a8, "HF664RT19V");
+		public static Car car4 = new Car(ferr_458, "CV33AC222A");
+		public static Car car5 = new Car(ford_mus, "J2K2N55CS4");
+		public static Car car6 = new Car(ford_foc, "PP32V15X81");
+
+		//example workers for showrooms
+		public static Worker worker1 = new Worker("Tim Smith", "755-125-345", 4500);
+		public static Worker worker2 = new Worker("Francois Lupo", "788-787-878", 3750);
+
+		//example clients
+		public static Client client1 = new Client("Dave Nowak", "699-911-914");
+		public static Client client2 = new Client("George Smith", "450-550-650");
+
+		//example showrooms
+		public static Showroom salon1 = new Showroom();
+		public static Showroom salon2 = new Showroom();
+
+		public static void Run()
+		{
+			Console.WriteLine("### PERFORMING TEST OPERATIONS ###");
+			// ### CONSOLE MUTE ###
+			var std = Console.Out;
+			Console.SetOut(TextWriter.Null);
+
+
+			//fill some data into instances
+			client1.AddCar(car6);
+
+			salon1.Authenticate(worker1, "kocham_Milene");
+			salon1.AddCar(car1);
+			salon1.AddCar(car2);
+			salon1.AddCar(car3);
+			salon1.AddCar(car4);
+			salon1.Clients.Add(client1);
+
+			//client1.CountCars();
+			//client1.PrintCars();
+			//worker1.CheckInfo();
+			//salon1.ListCars();
+			//salon1.RemoveCar(car2);
+			//salon1.ListCars();
+			//salon1.ContactClients();
+
+			// ### CONSOLE UNMUTE ###
+			Console.SetOut(std);
+			Console.WriteLine("### TEST OPERATIONS FINISHED ###\n");
+
+
+		}
+	}
+}
--- CarShowroom/Program.cs
+++ CarShowroom/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using CarShowroom.Database;
+using CarShowroom.Examples;
 using CarShowroom.Logic;
 
 namespace CarShowroom
@@ -8,57 +8,11 @@ namespace CarShowroom
 	{
 		private static void Main()
 		{
-			var contact1 = new Person("John Neuer", "601-602-603");
-			var contact2 = new Person("Stefano Gino", "343-121-777");
+			//prepare some example data to work with, a basic database mock
+			TestDataset.Run();
 
-			var bmw = new CarBrand(1, "BMW", "Germany", contact1);
-			var audi = new CarBrand(2, "Audi", "Germany", contact1);
-			var ferrari = new CarBrand(3, "Ferrari", "Italy", contact2);
-
-			var model1 = new CarModel(bmw, "E32", 1992);
-			var model2 = new CarModel(audi, "A8", 2011);
-			var model3 = new CarModel(ferrari, "458", 2013);
-
-			var car1 = new Car(model1, "A1B22C9123");
-			var car2 = new Car(model2, "G34FG21EF7");
-			var car3 = new Car(model3, "HF664RT19V");
-
-			Console.WriteLine("\n");
-
-			var client1 = new Client("Dave Nowak", "699-911-914");
-			client1.AddCar(car2);
-			client1.AddCar(car3);
-
-			client1.CountCars();
-			client1.PrintCars();
-
-			Console.WriteLine("\n");
-
-			var worker1 = new Worker("Tim Smith", "755-125-345", 4500);
-			worker1.CheckInfo();
-
-			Console.WriteLine("\n");
-
-			var salon1 = new Showroom();
-			salon1.Authenticate(worker1, "kocham_Milene");
-			salon1.AddCar(car1);
-			salon1.AddCar(car2);
-			salon1.AddCar(car3);
-			salon1.ListCars();
-
-			Console.WriteLine("\n");
-
-			salon1.RemoveCar(car2);
-			salon1.ListCars();
-
-			Console.WriteLine("\n");
-
-			salon1.Clients.Add(client1);
-			salon1.ContactClients();
-
-			Console.WriteLine("\n");
 			var manager = new ShowroomManager();
-			manager.SellCar(client1, salon1, 2);
+			manager.SellCar(TestDataset.client1, TestDataset.salon1, 3);
 
 			//idle
 			Console.ReadKey();
